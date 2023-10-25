7ea6de79d214233ec8b9dbb2845323757c81edaf
noah <noahspottdev@gmail.com>
2023-10-25 00:05:34 -0400

    updated midi

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-from gpiozero import LED
 from gpiozero import Button
-# import foxdot
-import time
-import sys
-import signal
-
-print(sys.path)
+from mido import Message, MidiFile, MidiTrack
+from time import sleep
+import fluidsynth
 
 # Keys are numbered left to right 1-5
-key1 = Button(26)
-key2 = Button(19)
-key3 = Button(13)
-key4 = Button(6)
-key5 = Button(5)
-keys = [key1, key2, key3, key4, key5]
+button_pins = [26, 19, 13, 6, 5]
+buttons = [Button(pin) for pin in button_pins]
 
-# synth = foxdot.Synth()
+midi_notes = [60, 62, 64, 65, 67]
 
-def print_current_time():
-    current_time = time.strftime("%H:%M:%S")
-    print("Current time:", current_time)        
+# sample_rate = 44100
 
-key1.when_pressed = print_current_time
-key2.when_pressed = print_current_time
-key3.when_pressed = print_current_time
-key4.when_pressed = print_current_time
-key5.when_pressed = print_current_time
+# fs = fluidsynth.Synth(sample_rate=sample_rate)
+# # fs.start(driver="alsa")
 
-try:
+# sf2_file = ""
+# # fs.load_soundfont(sf2_file)
+
+def map_buttons_to_midi():
+    for button, note in zip(buttons, midi_notes):
+        button.when_pressed = lambda note=note: play_midi_note_on(note)
+        button.when_released = lambda note=note: play_midi_note_off(note)
+
     while True:
-        pass
-except KeyboardInterrupt:
+        sleep(0.1)  # Add a small delay
 
-    print("Program Terminated")
+# Function to play a MIDI note on
+def play_midi_note_on(note, velocity=64):
+    print(f'NOTE ON \nnote: {note}, velocity: {velocity}')
+    # fs.noteon(0, note, velocity)
+
+# Function to play a MIDI note off
+def play_midi_note_off(note, velocity=0):
+    print(f'NOTE OFF \nnote: {note}, velocity: {velocity}')
+    # fs.noteoff(velocity, note)
+
+if __name__ == '__main__':
+    try:
+        map_buttons_to_midi()
+    except KeyboardInterrupt:
+        pass
